b3ec319bb0a84a48ea212276d618c2996145a577
kenhua-l <l_kenhua93@hotmail.com>
2025-12-12 21:52:30 +0800

    add mrt map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -7,7 +7,7 @@ export default function Map() {
   return (
     <>
       <Head>
-        <title>Map</title>
+        <title>MRT Map</title>
         <meta name="description" content="New Map" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
